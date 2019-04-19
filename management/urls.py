432e73d6eb168c2bf43108a3667c8533908c1fce
jacktnp <thanaponpkw@gmail.com>
2019-04-19 23:41:15 +0700

    :snowflake: update more items
    - add path /bill
    - add path /payment
    - add path /report
    - add template code
    - fixed title name login&register page
    - fixed koala.css
    - remove quick menu
    - fixed navbar link

--- management/urls.py
+++ management/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.user_home, name='user_index'),
     path('login/', views.login_page, name='login_page'),
     path('register/', views.register_page, name='register_page'),
+    path('annouce/', views.user_annouce, name='user_annouce'),
+    path('bill/', views.user_bill, name='user_bill'),
+    path('payment/', views.user_payment, name='user_payment'),
+    path('report/', views.user_report, name='user_report'),
 ]
--- management/views.py
+++ management/views.py
@@ -3,11 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def user_home(request):
-    return render(request, template_name='member/index.html')
-
 def login_page(request):
     return render(request, template_name='dormlab/login.html')
 
 def register_page(request):
     return render(request, template_name='dormlab/register.html')
+
+def user_home(request):
+    return render(request, template_name='member/index.html')
+
+def user_annouce(request):
+    return render(request, template_name='member/annouce.html')
+
+def user_bill(request):
+    return render(request, template_name='member/bill.html')
+
+def user_payment(request):
+    return render(request, template_name='member/payment.html')
+
+def user_report(request):
+    return render(request, template_name='member/report.html')
