47d181e6f401d67ecc45e7c0c348af27547ba71e
prachya <60070046@kmitl.ac.th>
2019-05-08 00:46:20 +0700

    add order and list per page :balloon:
    add order and list per page

--- management/admin.py
+++ management/admin.py
@@ -27,16 +27,16 @@ from .forms import GuestCreateForm
 
 class DormAdmin(admin.ModelAdmin):
     list_display=['dorm_name','floor','room_amount','location']
-    search_fields = ['room_number']
-    list_per_page = 10
+    search_fields = ['room_amount','floor','location','dorm_name']
+    list_per_page = 20
 
 
 class RoomAdmin(admin.ModelAdmin):
     list_display=['dorm_dorm_id','room_number','room_floor','room_type']
     list_filter = ['dorm_dorm_id',  'room_floor', 'room_type','status']
-    search_fields = ['room_number']
+    search_fields = ['room_number','room_type', 'room_floor']
     ordering = ['room_number']
-
+    list_per_page = 20
 
 
 
@@ -45,19 +45,23 @@ class ContractingAdmin(admin.ModelAdmin):
     # pang2 = Dorm.objects.get(pk="dorm_name")
     list_display=['guest_guest_id','room_room_id','expire_date']
     list_filter = [ 'expire_date']
-    search_fields = ['room_number',"guest_guest_id"]
+    search_fields = ['expire_date']
+    ordering = ['expire_date']
+    list_per_page = 20
 
 class ReportingAdmin(admin.ModelAdmin):
     list_display=['room_room_id','report_type_type_id','report_desc','report_date']
     list_filter = ['report_type_type_id',  'report_date']
-    search_fields = ['room_number']
+    #search_fields = ['report_date']
     ordering = ['-report_date']
+    list_per_page = 20
 
 class EmployeeAdmin(UserAdmin):
     list_display=['dorm_dorm_id',"username","emp_phone"]
     list_filter = ['dorm_dorm_id']
-  #  search_fields = ["emp_phone"]
-
+    search_fields = ["username","emp_phone"]
+    ordering = ['dorm_dorm_id']
+    list_per_page = 20
     add_fieldsets = (
         (
             None, {
@@ -78,13 +82,16 @@ class EmployeeAdmin(UserAdmin):
 class ExpenseAdmin(admin.ModelAdmin):
     list_display=['expense_dorm_id', 'exp_desc','price_per_unit']
     list_filter = ['expense_dorm_id']
-    search_fields = ['expense_dorm_id', 'exp_desc','price_per_unit']
+    search_fields = [ 'exp_desc','price_per_unit']
+    ordering = ['expense_dorm_id']
+    list_per_page = 20
 
 class GuestAdmin(UserAdmin):
     list_display=['username','phone','address']
-    #list_filter = ['dorm_dorm_id', 'owner']
-    search_fields = ['username', 'phone']
-
+    list_filter = []
+    search_fields = ['username','phone','address']
+    ordering = ['username']
+    list_per_page = 20
     add_fieldsets = (
         (
             None, {
@@ -105,30 +112,42 @@ class GuestAdmin(UserAdmin):
 class InvoiceAdmin(admin.ModelAdmin):
     list_display=['contracting_contract_id','invoice_date','total','status']
     list_filter = ['invoice_date', 'status']
-  # search_fields = ['contracting_contract_id', 'status']
+    search_fields = ['invoice_date','total','status']
+    ordering = ['status']
+    list_per_page = 20
     # change_list_template = 'admin/chart.html'
     # date_hierarchy = 'created'
 
 
 
 class Invoice_detailAdmin(admin.ModelAdmin):
-    list_display=['invoice_invoice_id','expense_exp_id','unit','__str__']
+    list_display=['invoice_invoice_id','expense_exp_id','unit']
+    ordering = ['invoice_invoice_id']
+    list_per_page = 20
+    #list_filter = ['invoice_invoice_id','expense_exp_id','unit']
+    #search_fields = ['invoice_invoice_id','expense_exp_id','unit']
 
 class ParcelAdmin(admin.ModelAdmin):
     list_display=['__str__','arrive_date','post_type',"track_number"]
     list_filter = ['arrive_date']
     search_fields = ['fname_guest', 'lname_guest', "track_number", "post_type"]
+    ordering = ['-arrive_date']
+    list_per_page = 20
 
 class NewAdmin(admin.ModelAdmin):
     list_display=["__str__",'news_title','news_content']
-    search_fields = ['news_title']
+    search_fields = ['news_title','news_content']
     list_filter = ['dorm_dorm_id']
+    ordering = ['dorm_dorm_id']
+    list_per_page = 20
+
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display=["payment_guest_id",'payment_desc','payment_datetime','payment_confirm', 'bill_picture']
-    search_fields = ["payment_guest_id",'payment_desc','payment_confirm']
+    search_fields = ['payment_desc','payment_datetime','payment_confirm', 'bill_picture']
     list_filter = ['payment_confirm', 'payment_datetime']
-
+    ordering = ['payment_confirm']
+    list_per_page = 20
 
 
 admin.site.register(Payment,PaymentAdmin)
